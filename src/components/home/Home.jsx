ce47953a34936f5747255cc7355c5f3dc157b521
Isaac Gonzalez <u1337x@gmail.com>
2025-02-09 17:37:28 -0500

    Input: Added input field to fetch email from API

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Home.scss'
+export default function Home() {
+  return (
+    <div className='Home'>
+        <div className='Home__container'>
+            <input className='Home__input' type="text" placeholder='GOT PWN ?'/>
+        </div>
+    </div>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import Navbar from './components/navbar/Navbar.jsx'
+import Home from './components/home/Home.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Navbar />
-    <App />
+    <Home />
   </StrictMode>,
 )
