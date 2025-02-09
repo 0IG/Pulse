0440293b66d232c7f350c88bbfe8660e2d93be55
Isaac Gonzalez <u1337x@gmail.com>
2025-02-09 16:45:52 -0500

    Navbar: Simple layout, more to add.

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Navbar.scss"
+export default function Navbar() {
+  return (
+    <div className='Navbar'>
+        <h1 className='Navbar__title'>PULSE</h1>
+        <img className='Navbar__icon' src="https://i.imgur.com/IlhF1bf.png" alt="Pulse-Icon" />
+    </div>
+  )
+}
