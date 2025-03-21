1506595a028ad994048818c8de98ecb7be4f3e50
Isaac Gonzalez <u1337x@gmail.com>
2025-03-21 13:22:23 -0400

    Update: Audit / Fixing packages

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './Home.scss'
+import { RiUserSearchFill } from "react-icons/ri";
+
 export default function Home() {
   return (
     <div className='Home'>
         <div className='Home__container'>
             <input className='Home__input' type="text" placeholder='GOT PWN ?'/>
+            {/* <button className='Home__button' type='submit'>
+                <RiUserSearchFill className='Home__icon'/>
+            </button> */}
         </div>
     </div>
   )
