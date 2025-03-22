08a6758dc7a8864b611a2a5ed1a969e2d813bcef
Isaac Gonzalez <u1337x@gmail.com>
2025-03-22 01:31:37 -0400

    Login: Boiler plate login

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,22 @@
+// Import libraries
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+// Import styles
 import './index.css'
+// Import components
 import Navbar from './components/navbar/Navbar.jsx'
 import Home from './components/home/Home.jsx'
-import Index from './components/index/Index.jsx'
-import { BrowserRouter } from 'react-router'
+import Login from './components/account/login.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <Router>
     <Navbar />
-    <Home />
-    <Index />
-    </BrowserRouter>
+    <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/login" element={<Login/>} />
+    </Routes>
+    </Router>
   </StrictMode>
 )
