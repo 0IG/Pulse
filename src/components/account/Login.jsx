ab9303ac17e81445b1011cfb0acf019e4745d783
Isaac Gonzalez <u1337x@gmail.com>
2025-03-22 15:57:07 -0400

    Login: Saving some style changes + edits

--- src/components/account/Login.jsx
+++ src/components/account/Login.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useNavigate } from 'react-router'
 import "./Login.scss"
-export default function login() {
+export default function Login() {
+    const navigate = useNavigate();
+    const [email, setEmail] = useState('');
+
+    const handleRedirect = () => {
+        e.preventDefault()
+        navigate('/signup')
+    }
   return (
     <div className='login'>
+        <form className='login__form'>
         <h1 className='login__title'>Login</h1>
-        <input className='login__input' placeholder='EMAIL'></input>
+        <input className='login__email' placeholder='EMAIL'></input>
+        <input className='login__password' placeholder='PASSWORD'></input>
+        </form>
     </div>
   )
 }
